0d291abde5be930d06ad1080375b6b20b29c39bc
thonguyen100 <haithonguyen0305@gmail.com>
2025-09-12 16:06:23 +0700

    Test

--- envs/stg/monitoring.tf
+++ envs/stg/monitoring.tf
@@ -10,6 +10,8 @@ module "monitoring_frontend" {
   asset_name    = module.cloud_run.cloud_run_frontend_service_name_v1
   asset_lbnref  = "${var.environment}-cloud-run-frontend"
   customer_name = var.customer_name
+
+  notify_to = "@slack-monitoring-team @webhook-monitoring-team-payload"
 }
 
 # Monitoring-backend
@@ -24,6 +26,8 @@ module "monitoring_backend" {
   asset_name    = module.cloud_run.cloud_run_backend_service_name_v1
   asset_lbnref  = "${var.environment}-cloud-run-backend"
   customer_name = var.customer_name
+
+  notify_to = "@slack-monitoring-team @webhook-monitoring-team-payload"
 }
 
 # Monitoring for the mysql db
@@ -38,18 +42,21 @@ module "monitoring_db" {
   asset_name    = module.database.db_instance_connection_name
   asset_lbnref  = "${var.environment}-db"
   customer_name = var.customer_name
+
+  notify_to = "@slack-monitoring-team @webhook-monitoring-team-payload"
+
 }
 
 # Monitoring for the test_cr
-# module "test_monitor" {
-#   source      = "../../modules/monitoring/cloudrun-dev"
-#   environment = var.environment
+module "test_monitor" {
+  source      = "../../modules/monitoring/cloudrun-dev"
+  environment = var.environment
 
-#   datadog_api_key = var.datadog_api_key
-#   datadog_app_key = var.datadog_app_key
+  datadog_api_key = var.datadog_api_key
+  datadog_app_key = var.datadog_app_key
 
-#   customer_id   = var.customer_id
-#   asset_name    = "${var.environment}-test-cr-by-terraform"
-#   asset_lbnref  = "${var.environment}-test-cr"
-#   customer_name = var.customer_name
-# }
+  customer_id   = var.customer_id
+  asset_name    = "${var.environment}-test-cr-by-terraform"
+  asset_lbnref  = "${var.environment}-test-cr"
+  customer_name = var.customer_name
+}
